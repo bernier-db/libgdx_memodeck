a13d29ec82912124169c66f530e152257ba43ca3
davebernier <bernier.db@gmail.com>
2017-12-10 22:05:35 -0500

    Adjusting config

--- core/src/main/java/com/mygdx/game/Config.java
+++ core/src/main/java/com/mygdx/game/Config.java
@@ -8,7 +8,7 @@ public class Config {
     public final static int MAX_ROUND = 5;
     public static int CURRENT_ROUND = 0;
     public static int POINTS = 0;
-    public final static int GOAL = 20;
+    public final static int GOAL = 8;
     public static int ROUNDS_LEFT = 5;
     public final static int NB_COINS = 40;
 
